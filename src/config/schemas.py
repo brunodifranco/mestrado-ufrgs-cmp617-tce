f6786902d01d9ef08117000713d14ee855fa40cd
Bruno Di Franco Albuquerque <brunodifranco99@gmail.com>
2024-03-28 21:51:13 -0300

    UPDATE: lda modeling

--- src/config/schemas.py
+++ src/config/schemas.py
@@ -1,12 +1,36 @@
 import polars as pl
 
+# raw_dtypes = {
+#     "CD_ORGAO": "object",
+#     "NM_ORGAO": "object",
+#     "NR_LICITACAO": "float64",
+#     "ANO_LICITACAO": "object",
+#     "CD_TIPO_MODALIDADE": "object",
+#     "NR_COMISSAO": "float64",
+#     "TP_OBJETO": "object",
+#     "CD_TIPO_FASE_ATUAL": "object",
+#     "TP_LICITACAO": "object",
+#     "TP_NIVEL_JULGAMENTO": "object",
+#     "TP_CARACTERISTICA_OBJETO": "object",
+#     "TP_NATUREZA": "object",
+#     "DS_OBJETO": "object",
+#     "VL_LICITACAO": "object",
+#     "BL_PERMITE_CONSORCIO": "object",
+#     "DT_ABERTURA": "object",
+#     "DT_HOMOLOGACAO": "object",
+#     "DT_ADJUDICACAO": "object",
+#     "BL_LICIT_PROPRIA_ORGAO": "object",
+#     "VL_HOMOLOGADO": "object",
+#     "DS_OBSERVACAO": "object",
+# }
+
 raw_dtypes = {
     "CD_ORGAO": "object",
     "NM_ORGAO": "object",
-    "NR_LICITACAO": "float64",
+    "NR_LICITACAO": "object",
     "ANO_LICITACAO": "object",
     "CD_TIPO_MODALIDADE": "object",
-    "NR_COMISSAO": "float64",
+    "NR_COMISSAO": "object",
     "TP_OBJETO": "object",
     "CD_TIPO_FASE_ATUAL": "object",
     "TP_LICITACAO": "object",
@@ -23,6 +47,7 @@ raw_dtypes = {
     "VL_HOMOLOGADO": "object",
     "DS_OBSERVACAO": "object",
 }
+
 
 
 raw_schema = {
